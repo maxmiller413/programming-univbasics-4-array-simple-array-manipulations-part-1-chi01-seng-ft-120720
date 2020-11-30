484840024c6815a9291726365383fa525783c037
Max Miller <maxmiller413@gmail.com>
2020-11-30 17:08:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,6 +6,6 @@ def using_unshift(array, string)
   array.unshift(string)
 end  
 
-def using_pop(colors_in_the_rainbow, next_color)
-  colors_in_the_rainbow.pop(next_color)
+def using_pop(array)
+  array.pop
 end
